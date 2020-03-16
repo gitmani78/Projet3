cec1d1832284c805c86e32d4469aaf11495ae404
Mani <suliman.shazad@gmail.com>
2020-03-16 13:38:33 +0100

    Initial Commit

---
+++ Projet2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet2
+//
+//  Created by Mani on 16/03/2020.
+//  Copyright © 2020 Mohammad Shazad. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
